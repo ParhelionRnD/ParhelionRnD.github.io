134b06d090e3da3c39219b74897389d07ce0af32
ParhelionRnD <parhelionrnd@gmail.com>
2025-11-16 04:15:13 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
-https://<ParhelionRnD>.github.io/<ParhelionRnD.github.io>/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/ParhelionRnD.github.io/", // or "/" depending on how you're hosting
+  plugins: [react()],
+});
